5a88a4aedccc97d735c31c83b927b118350f2ebb
DT Pham <dt99.pham@gmail.com>
2022-04-29 19:09:00 -0700

    Initial Commit

---
+++ Game2048/Game2048App.swift
@@ -0,0 +1,17 @@
+//
+//  Game2048App.swift
+//  Game2048
+//
+//  Created by Duong Pham on 4/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Game2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
